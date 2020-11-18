3f168f41d29cc716ba1f418bad2d05e0aa20004a
Arthur Young <arthuryoung@g.harvard.edu>
2020-11-18 13:15:16 -0500

    still no parser, but a tough test case is defined and validated

--- bin/class_call_main.py
+++ bin/class_call_main.py
@@ -19,48 +19,31 @@ def main():
 
 
 def build_function(function_string_in, function_list_in):
-    op_string = ["+", "-", "*", "/", "**"]
+    op_string = ["+", "-", "*", "/", "^"]
     brack_string = ["[", "]", "(", ")", "{", "}"]
     trimmed_string = ""
     for i in function_string_in:
         if i != " ":
             trimmed_string = trimmed_string + i
-            if i in op_string:
-                print(i, i in op_string)
-            if i in brack_string:
-                print(i, i in op_string)
-
-
+            # if i in op_string:
+            #     print(i, i in op_string)
+            # if i in brack_string:
+            #     print(i, i in op_string)
     print(trimmed_string)
 
 
     ## handling the definition and specification of the library variables
-    check_string1 = 'sin(x)'
-    check_string2 = 'cos(x)'
-    check_string3 = 'exp(x)'
-    check_string4 = 'tan(x)'
-    x1 = CMobject(0)
-    x2 = CMobject(np.pi)
-    x3 = np.pi
-    f1 = FuncObj(check_string1, x1)
-    f2 = FuncObj(check_string2, x1)
-    f3 = FuncObj(check_string2, x3)
-    print("f1 value, der: {}, {}".format(f1.val, f1.der) )
-    print("f2 value, der: {}, {}".format(f2.val, f2.der) )
-    print("f3 value, der: {}, {}".format(f3.val, f3.der) )
+    sin_string = 'sin(x)'
+    cos_string = 'cos(x)'
+    exp_string = 'exp(x)'
+    tan_string = 'tan(x)'
+    log_string = 'ln(x)'
 
-    seed = CMobject(1.0)
-    f4 = 2*FuncObj(check_string1, seed) + ((FuncObj(check_string3, seed))/(4*FuncObj(check_string4, 3*seed)))
-    f5 = 2*FuncObj(check_string1, seed)
-    f6 = (FuncObj(check_string3, seed))/(4*FuncObj(check_string4, 3*seed))
-    f7 = FuncObj(check_string1, FuncObj(check_string4, FuncObj(check_string3, seed)))
-    f8 = FuncObj(check_string1, seed)**(FuncObj(check_string4, seed)) + 2**(FuncObj(check_string2, seed)) + FuncObj(check_string1, seed)**(FuncObj(check_string4, seed))**(FuncObj(check_string3, seed))
+    x1 = CMobject(1.0)
 
-    print("f4 value, deriv: {}, {}".format(f4.val, f4.der) )
-    print("f5 value, deriv: {}, {}".format(f5.val, f5.der) )
-    print("f6 value, deriv: {}, {}".format(f6.val, f6.der) )
-    print("f7 value, deriv: {}, {}".format(f7.val, f7.der) )
-    print("f8 value, deriv: {}, {}".format(f8.val, f8.der) )
+    ## the following is a test case for: sin(tan(x)) + 2^(cos(x)) + sin(x)^tan(x)^exp(x) - (cos(x))^2, seeded at x = 1. Try it in autograd, it works.
+    test_func1 = FuncObj(sin_string, FuncObj(tan_string, x1)) + 2**(FuncObj(cos_string, x1)) + FuncObj(sin_string, x1)**(FuncObj(tan_string, x1))**(FuncObj(exp_string, x1)) - FuncObj(cos_string, x1)**2
+    print("test_func1 val, der: {}, {}".format(test_func1.val, test_func1.der))
 
 if __name__ == "__main__":
     main()
