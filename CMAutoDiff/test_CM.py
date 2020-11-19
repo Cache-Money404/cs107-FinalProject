abcdda94063fd77cc1240249a73668586771e72b
John Scudder <jscud23@gmail.com>
2020-11-19 04:41:19 -0500

    updated test_CM to increased code coverage; updated gitignore

--- CMAutoDiff/test_CM.py
+++ CMAutoDiff/test_CM.py
@@ -17,9 +17,19 @@ def test_mul_operation():
     
 def test_add_sub_operations():
     x = CMobject(1)
-    f = x + 2 + x - 1 - x
+    f = x + 2 + x + x + x - 1 - x - x - x
     assert (f.val,f.der) == (2.0, 1.0)
     
+def test_radd_operation():
+    x = CMobject(1)
+    f = 2 + x
+    assert (f.val,f.der) == (3.0, 1.0)
+    
+def test_rsub_operation():
+    x = CMobject(1)
+    f = 10 - x
+    assert (f.val,f.der) == (9.0, -1.0)
+
 def test_div_operations():
     x = CMobject(1)
     f = x/(3*x + 1) - x/5 + 5/x
@@ -27,8 +37,8 @@ def test_div_operations():
     
 def test_pow_operations():
     x = CMobject(1)
-    f = x**2 + 2**x
-    assert (f.val,f.der) == (3.0, 3.386294361119891)
+    f = x**2 + 2**x + x**x**x**2**x
+    assert (f.val,f.der) == (4.0, 4.386294361119891)
     
 def test_difficult_derivative_case():    
     x1 = CMobject(1.0)
@@ -38,10 +48,40 @@ def test_difficult_derivative_case():
     print("test_func1 val, der: {}, {}".format(test_func1.val, test_func1.der))
     assert (test_func1.val,test_func1.der) == ( 2.724678163898656, -1.0139897786023675)
     
-def test_value_error():
+def test_object_input_error():
     with pytest.raises(ValueError):
         CMobject(1, 'fake')
+        
+def test_FuncObj_constant():
+    f = FuncObj('sin',2)
+    assert (f.val,f.der) == (0.9092974268256817, -0.0)
     
+def test_natural_log_function_library():
+    x = CMobject(2)
+    f = FuncObj('log',x)
+    assert (f.val,f.der) == (0.6931471805599453, 0.5)
+    
+def test_newtons_method():
+    # define newton's method to work with CMobject and FuncObj
+    def newt(f,x,tol= 1e-10,max_it=100):
+        
+        for i in range(max_it):
+            dx = -f(x).val/f(x).der # Update Delta x_{k}
+            if np.abs(dx) < tol: # Stop iteration if solution found
+                print(f"root found at: x={x.val} after {i+1} iterations.")
+                root = x.val
+                return root
+                break
+            else:
+                x += dx #update x
+    
+    # TEST CASE FOR f(x) = x^2 + ln(x) + x
+    def f(x): # define function
+        return x**2 + FuncObj('log',x) + x
 
-if __name__ == "__main__":
-    print("Here")
+    x = CMobject(1) 
+    result = newt(f,x)
+    assert result == 0.4858388639605664
+
+#if __name__ == "__main__":
+#    print("Here")
