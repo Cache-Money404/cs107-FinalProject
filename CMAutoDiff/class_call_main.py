0aa5ca60b795c7198ba13c82a31b5db960d6426e
John Scudder <jscud23@gmail.com>
2020-11-18 20:29:57 -0500

    Updated class_call_main, function_library, and CMtest
    class_call_main : commented out majority of main() function.  We will not include this as a required file for milestone2.  Will implement for final project when parser works correctly
    
    function_library: changed inputs from 'sin(x)' to 'sin'
    
    CMtest: added Arthur's complex test case

--- CMAutoDiff/class_call_main.py
+++ CMAutoDiff/class_call_main.py
@@ -3,19 +3,19 @@ from CMobject import CMobject
 from FuncObj import FuncObj
 import numpy as np
 
-def main():
-    with open('Read_in1.txt', 'r') as file:
-        function_string_main = file.read().replace('\n', '')
+#def main():
+    # with open('Read_in1.txt', 'r') as file:
+    #     function_string_main = file.read().replace('\n', '')
 
-    with open('Read_in2.txt', 'r') as file:
-        function_list_raw = file.read().replace('\n', '')
+    # with open('Read_in2.txt', 'r') as file:
+    #     function_list_raw = file.read().replace('\n', '')
 
-    print(function_string_main)
-    print(function_list_raw)
+    # print(function_string_main)
+    # print(function_list_raw)
 
-    function_list_main = function_list_raw.split()
+    #function_list_main = function_list_raw.split()
 
-    build_function(function_string_main, function_list_main)
+    #build_function(function_string_main, function_list_main)
 
 
 def build_function(function_string_in, function_list_in):
@@ -33,17 +33,17 @@ def build_function(function_string_in, function_list_in):
 
 
     ## handling the definition and specification of the library variables
-    sin_string = 'sin(x)'
-    cos_string = 'cos(x)'
-    exp_string = 'exp(x)'
-    tan_string = 'tan(x)'
-    log_string = 'ln(x)'
+    sin = 'sin'
+    cos = 'cos'
+    exp = 'exp'
+    tan = 'tan'
+    log = 'ln'
 
     x1 = CMobject(1.0)
 
     ## the following is a test case for: sin(tan(x)) + 2^(cos(x)) + sin(x)^tan(x)^exp(x) - (cos(x))^2, seeded at x = 1. Try it in autograd, it works.
-    test_func1 = FuncObj(sin_string, FuncObj(tan_string, x1)) + 2**(FuncObj(cos_string, x1)) + FuncObj(sin_string, x1)**(FuncObj(tan_string, x1))**(FuncObj(exp_string, x1)) - FuncObj(cos_string, x1)**2
+    test_func1 = FuncObj(sin, FuncObj(tan, x1)) + 2**(FuncObj(cos, x1)) + FuncObj(sin, x1)**(FuncObj(tan, x1))**(FuncObj(exp, x1)) - FuncObj(cos, x1)**2
     print("test_func1 val, der: {}, {}".format(test_func1.val, test_func1.der))
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
