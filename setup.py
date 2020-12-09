d82c48d6faa6829f6d13f71c41f52b4d0a1a3b4a
Paul Tembo <56775709+feetha05@users.noreply.github.com>
2020-12-08 19:41:52 -0500

    Update for setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
-
+​
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
+​
 setuptools.setup(
-    name="Cache-Money404", 
+    name="Cache-Money404",
     version="1.0.0",
     author="Paul Tembo, Junyi Guo, Matthew Hawes, Jack Scudder , Arthur Young",
     author_email="",
@@ -12,12 +12,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Cache-Money404/cs107-FinalProject",
-    package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src', exclude=('tests',)),
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.16.3']
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
