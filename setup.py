6aff3beb02868f560fc47c09f3a64903064e411c
Jessieguojunyi <jessieguo.guo@gmail.com>
2020-12-10 14:48:57 -0500

    Update setup file for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
-​
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
-​
+
 setuptools.setup(
-    name="Cache-Money404",
-    version="1.0.0",
+    name="CMFluid",
+    version="0.0.1",
     author="Paul Tembo, Junyi Guo, Matthew Hawes, Jack Scudder , Arthur Young",
     author_email="",
     description="cs207 Final Project and implementation of Auto Differentiaion",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Cache-Money404/cs107-FinalProject",
     packages=setuptools.find_packages(),
-    install_requires=['numpy==1.16.3']
+    install_requires=['numpy==1.16.3'], 
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
