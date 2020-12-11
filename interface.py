9b187d9d8e0be91ff15b45f5c1aa77865ced0c24
Paul Tembo <56775709+feetha05@users.noreply.github.com>
2020-12-11 01:56:35 -0500

    Add files via upload

--- interface.py
+++ interface.py
@@ -8,129 +8,113 @@ def Interface():
             stop = 1
         else:
             if input1 == 1: #Uniform
-                string2 = "Enter A as this is the letter that coresponds to function Uniform:"
-                input2 = str(input(string2))
-                if input2 =="A" or input2=='a': 
-                    d = dict();
-                    d['Property'] = "A"
-                    d['function'] = "Uniform"
-                    return d
-                else:
-                    raise Exception("Invalid input")
+                string2 = "Enter Strength?:"
+                input2 = float(input(string2))
+                d = dict();
+                d['Strength'] = input2
+                d['function'] = "Uniform"
+                return d
 
             elif input1 == 2: # Doublet
-                string2 = "Enter Q as this is the letter that coresponds to function Doublet:"
-                input2 = str(input(string2))
-
-                if input2 =="Q" or input2=='q':
-                    string3  = "Enter X Value"
-                    inputx = float(input(string3))
-                    string4  = "Enter Y Value"
-                    inputy = float(input(string4))
-                    d = dict();
-                    d['X'] = inputx
-                    d['Y'] = inputy
-                    d['Property'] = "Q"
-                    d['function'] = "Doublet"
-
-                    return d
-                else:
-                    raise Exception("Invalid input")
-
+                string2 = "Enter Strength?:"
+                input2 = float(input(string2))
+                string3  = "Enter X Value"
+                inputx = float(input(string3))
+                string4  = "Enter Y Value"
+                inputy = float(input(string4))
+                d = dict();
+                d['X'] = inputx
+                d['Y'] = inputy
+                d['Strength'] = input2
+                d['function'] = "Doublet"
+                return d
             elif input1 == 3: # Sink
-                string2 = "Enter Q as this is the letter of the property that coresponds to function Sink:"
+                string2 = "Enter Strength?:"
                 input2 = str(input(string2))
 
-                if input2 =="Q" or input2=='q':
-                    string3  = "Enter X Value"
-                    inputx = float(input(string3))
-                    string4  = "Enter Y Value"
-                    inputy = float(input(string4))
+                string5  = "Enter Length"
+                input3 = float(input(string5))
 
-                    d = dict();
-                    d['X'] = inputx
-                    d['Y'] = inputy
-                    d['Property'] = "Q"
-                    d['function'] = "Sink"
+                string3  = "Enter X Value"
+                inputx = float(input(string3))
+                string4  = "Enter Y Value"
+                inputy = float(input(string4))
+                d = dict();
+                d['X'] = inputx
+                d['Y'] = inputy
+                d['Strength'] = input2
+                d['Length'] = input3
+                d['function'] = "Sink"
 
-                    return d
-                else:
-                    raise Exception("Invalid input")
-
+                return d
             elif input1 == 4: # Source
-                string2 = "Enter Q as this is the letter of the property that coresponds to function Source:"
+                string2 = "Enter Strength?:"
                 input2 = str(input(string2))
 
-                if input2 =="Q" or input2=='q':
-                    string3  = "Enter X Value"
-                    inputx = float(input(string3))
-                    string4  = "Enter Y Value"
-                    inputy = float(input(string4))
 
-                    d = dict();
-                    d['X'] = inputx
-                    d['Y'] = inputy
-                    d['Property'] = "Q"
-                    d['function'] = "Source"
-                    return d
-                else:
-                    raise Exception("Invalid input")
+                string5  = "Enter Length"
+                input3 = float(input(string5))
+                
+                string3  = "Enter X Value"
+                inputx = float(input(string3))
+                string4  = "Enter Y Value"
+                inputy = float(input(string4))
 
+                d = dict();
+                d['X'] = inputx
+                d['Y'] = inputy
+                d['Strength'] = input2
+                d['Length'] = input3
+                return d
             elif input1 == 5: #Sphere
-                string2 = "Enter R as this is the letter of the property that coresponds to function Sphere:"
+                string2 = "Enter Strength?:"
                 input2 = str(input(string2))
+            
+                string3  = "Enter X Value"
+                inputx = float(input(string3))
+                string4  = "Enter Y Value"
+                inputy = float(input(string4))
 
-                if input2 =="R" or input2 =='r':
-                    string3  = "Enter X Value"
-                    inputx = float(input(string3))
-                    string4  = "Enter Y Value"
-                    inputy = float(input(string4))
-
-                    d = dict();
-                    d['X'] = inputx
-                    d['Y'] = inputy
-                    d['Property'] = "R"
-                    d['function'] = "Sphere"
-                    return d                
-                else:
-                    raise Exception("Invalid input")
-
+                d = dict();
+                d['X'] = inputx
+                d['Y'] = inputy
+                d['Strength'] = input2
+                d['function'] = "Sphere"
+                return d                
+    
             elif input1 == 6: # Vortex
-                string2 = "Enter Q as this is the letter of the property that coresponds to function Vortex:"
+                string2 = "Enter Strength?:"
                 input2 = str(input(string2))
-                if input2 =="R" or input2 =='r':
-                    string3  = "Enter X Value"
-                    inputx = float(input(string3))
+                
+                string3  = "Enter X Value"
+                inputx = float(input(string3))
 
-                    string4  = "Enter Y Value"
-                    inputy = float(input(string4))
+                string4  = "Enter Y Value"
+                inputy = float(input(string4))
 
-                    d = dict();
-                    d['X'] = inputx
-                    d['Y'] = inputy
-                    d['Property'] = "R"
-                    d['function'] = "Vortex"
-                    return d                
-                else:
-                    raise Exception("Invalid input")
+                d = dict();
+                d['X'] = inputx
+                d['Y'] = inputy
+                d['Strength'] = input2
+                d['function'] = "Vortex"
+                return d                
 
             elif input1 == 7: # Corner
-                string2 = "Enter Q as this is the letter of the property that coresponds to function Corner:"
+                string2 = "Enter Scale ?:"
                 input2 = str(input(string2))
-                if input2 =="R" or input2=='r':
-                    string3  = "Enter X Value"
-                    inputx = float(input(string3))
+            
+                string3  = "Enter X Value"
+                inputx = float(input(string3))
 
-                    string4  = "Enter Y Value"
-                    inputy = float(input(string4))
+                string4  = "Enter Y Value"
+                inputy = float(input(string4))
 
-                    d = dict();
-                    d['X'] = inputx
-                    d['Y'] = inputy
-                    d['Property'] = "A"
-                    d['function'] = "Corner"
-                    return d
-
-                else:
-                    raise Exception("Invalid input")
+                d = dict();
+                d['X'] = inputx
+                d['Y'] = inputy
+                d['Scale'] = input2
+                d['function'] = "Corner"
+                return d
+            else:
+                raise Exception("Invalid input")
         return print('Exiting out of function')
