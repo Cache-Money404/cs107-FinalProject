15558604a63dddba4bff2bfcddb570f372fe730f
Jessieguojunyi <jessieguo.guo@gmail.com>
2020-12-12 08:32:27 -0500

    Update ui and CMflow

--- CMAutoDiff/CMflow.py
+++ CMAutoDiff/CMflow.py
@@ -59,7 +59,6 @@ class Flow():
         self._pos = np.array([0, 0])
         self._key = key
         self.CMGs = []
-        ##### Changed Here because I did not find self._points ########
         self._points = np.array([])
     def rule_out_points(self, cart_coords: np.array):
         return cart_coords
@@ -98,7 +97,6 @@ class source(Flow):
         self._key = key
         self.b = 1  ## for now
         self.CMGs = []
-        ##### Changed Here because I did not find self._points ########
         self._points = np.array([])
 
     def rule_out_points(self, cart_coords: np.array):
@@ -139,7 +137,6 @@ class vortex(Flow):
         self._pos = np.array([inputs[1], inputs[2]])
         self._key = key
         self.CMGs = []
-        ##### Changed Here because I did not find self._points ########
         self._points = np.array([])
 
 
@@ -183,7 +180,6 @@ class tornado(source):
         self._pos = np.array([inputs[2], inputs[3]])
         self._key = key
         self.CMGs = []
-        ##### Changed Here because I did not find self._points ########
         self._points = np.array([])
 
     def compute_flow(self): ## assumes, for now, unitary b
