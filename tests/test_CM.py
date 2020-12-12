2fccb77eae34ca1b6c820079e0c2e139e004238c
Arthur Young <arthuryoung@g.harvard.edu>
2020-12-11 20:59:53 -0500

    fixed test cases, tanh and difficult thing

--- tests/test_CM.py
+++ tests/test_CM.py
@@ -25,7 +25,7 @@ import CMAutoDiff.CMflow as CMflow
 def test_repr_CMG():
     x = CMG(3, np.array([1,0]))
     assert x.__repr__() == 'CMGobject(val = 3.0, grad = [1 0])'
-    
+
 def test_object_input_error_CMG():
     with pytest.raises(ValueError):
         CMG('fake', np.array([0,1]))
@@ -46,7 +46,7 @@ def test_mul_CMG():
 def test_add_sub_operations_CMG():
     x = CMG(1, np.array([1,0]))
     f = x + 2 + x + x + x - 1 - x - x - x
-    f2 = 2 + x 
+    f2 = 2 + x
     f3 = 10 - x
     assert f.val == 2.0
     assert np.array_equal(f.grad,np.array([1., 0.]))
@@ -60,7 +60,7 @@ def test_div_operations_CMG():
     f = x/(3*x + 1) - x/5 + 5/x
     assert f.val == 5.05
     assert np.array_equal(f.grad,np.array([-5.1375, 0.]))
-    
+
 def test_pow_operations_CMG():
     x = CMG(1, np.array([1,0]))
     f = x**2 + 2**x + x**x**x**2**x
@@ -70,67 +70,67 @@ def test_pow_operations_CMG():
     assert np.array_equal(f.grad,np.array([4.386294361119891, 0.]))
     assert f2.val == 27.0
     assert np.array_equal(f2.grad,np.array([56.66253179403897, 0.]))
-    
+
 def test_negation_CMG():
     x = CMG(1, np.array([0,1]))
     y = -x
     assert y.val == -1.0
     assert np.array_equal(y.grad,np.array([0, -1]))
-    
+
 
 ############### TESTS FOR CMfunc ###############
-    
+
 def test_sin():
     x = CMG(2)
     f = CMfunc.sin(x)
     assert f.val == np.sin(2)
     assert np.array_equal(f.grad,np.array([np.cos(2)]))
     #print('passed sine test')
-    
+
 def test_arcsin():
     x = CMG(0.5)
     f = CMfunc.arcsin(x)
     assert (f.val,f.grad) == (np.arcsin(.5), (1-.5**2)**(-0.5))
     #print('passed arcsine test')
-    
+
 def test_arccos():
     x = CMG(0.5)
     f = CMfunc.arccos(x)
     assert f.val == np.arccos(.5)
     assert np.array_equal(f.grad,np.array([-(1-.5**2)**(-0.5)]))
     #print('passed arccosine test')
-    
+
 def test_arctan():
     x = CMG(3)
     f = CMfunc.arctan(x)
     assert f.val == np.arctan(3)
     assert np.array_equal(f.grad,np.array([(1+3**2)**(-2)]))
     #print('passed arctangent test')
-    
+
 def test_logistic():
     x = CMG(2,np.array([3,0]))
     f = CMfunc.logistic(x)
     assert f.val == 0.8807970779778823
     assert np.array_equal(f.grad,np.array([0.3149807562105195, -0.]))
     #print('passed logistic test')
-    
+
 def test_tanh():
-    x = CMG(2, np.array([1.,0]))
+    x = CMG(2., np.array([1.,0.], dtype=np.double ))
     f = CMfunc.tanh(x)
     assert f.val == 0.964027580075817
-    assert np.array_equal(f.grad, np.array([0.07065082485316432, 0.]))
+    assert np.array_equal(f.grad, np.array([0.0706508248531643, 0.]))
     # (np.tanh(2), np.cosh(2)**(-2))
     #print('passed hyperbolic tangent (and therefore sinh and cosh)')
-    
 
-# def test_difficult_derivative_case():
-#     x1 = CMG(1.0)
+
+def test_difficult_derivative_case():
+    x1 = CMG(1.0)
 
 #     ## the following is a test case for: sin(tan(x)) + 2^(cos(x)) + sin(x)^tan(x)^exp(x) - (cos(x))^2, seeded at x = 1. Try it in autograd, it works.
-#     test_func1 = CMfunc.sin(CMfunc.tan(x1)) + 2**(CMfunc.cos(x1)) + CMfunc.sin(x1)**CMfunc.tan(x1)**CMfunc.exp(x1) - CMfunc.cos(x1)**2
-#     print("test_func1 val, der: {}, {}".format(test_func1.val, test_func1.grad))
-#     assert (test_func1.val,test_func1.grad) == ( 2.724678163898656, -1.0139897786023675)
-#     print("Difficult derivative test passed.")
+    test_func1 = CMfunc.sin(CMfunc.tan(x1)) + 2**(CMfunc.cos(x1)) + CMfunc.sin(x1)**CMfunc.tan(x1)**CMfunc.exp(x1) - CMfunc.cos(x1)**2
+    print("test_func1 val, der: {}, {}".format(test_func1.val, test_func1.grad))
+    assert (test_func1.val,test_func1.grad) == ( 2.724678163898656, -1.0139897786023675)
+    print("Difficult derivative test passed.")
 
 
 def test_CMfunc_constant():
@@ -142,7 +142,7 @@ def test_CMfunc_constant():
     assert CMfunc.arctan(.1) == np.arctan(.1)
     assert CMfunc.exp(3) == np.exp(3)
     assert CMfunc.log(74088,42) == np.log(74088)/np.log(42) #using alternative base
-    
+
     print('passed constants test')
 
 def test_log_CMfunc():
@@ -154,11 +154,11 @@ def test_log_CMfunc():
     assert f.val== 0.6931471805599453
     assert np.array_equal(f.grad, np.array([0.5]))
     assert res == 3.0
-    
+
     #print('passed log test')
-    
+
 def test_sqrt():
-    x = CMG(2) 
+    x = CMG(2)
     f = CMfunc.sqrt(x)
     assert f.val == 2**.5
     assert np.array_equal(f.grad, np.array([0.5*(2**(-.5))]))
@@ -188,7 +188,7 @@ def test_newtons_method():
 
 
 
-    
+
 ############### TESTS FOR CMvector ###############
 
 
@@ -220,14 +220,14 @@ def test_all():
     test_arctan()
     test_logistic()
     test_tanh()
-    #test_difficult_derivative_case()
+    test_difficult_derivative_case()
     test_CMfunc_constant()
     test_log_CMfunc()
     test_sqrt()
     test_newtons_method()
     print('..all CMfunc tests run successfully!')
-    
-    
+
+
 test_all()
 
 # if __name__ == "__main__":
